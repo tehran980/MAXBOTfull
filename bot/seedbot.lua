a160521a939e16f016b24b6a1c06983d7e42fc96
tehran980 <hsn.m@torkmail.ir>
2016-01-09 08:40:23 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,25 +221,15 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {93367845,128729099,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {data = 'data/moderation.json'},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
-https://github.com/SEEDTEAM/TeleSeed
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-Our channels
-@teleseedch [English]
+@tehran98
+[English]
 ]],
     help_text = [[
 Commands list :
